f89c06d69044031df8bffeed6ff071fe5cf63c7a
maurosakugawa <maurotoshiuki@gmail.com>
2026-05-20 12:50:54 -0300

    chore: estrutura inicial modular do Smart Planner

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <button className="btn btn-primary">
+        Smart Planner
+      </button>
+    </div>
+  )
+}
+
+export default App
